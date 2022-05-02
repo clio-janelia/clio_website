ebebc3d86b38e9f0366af5fe1b6fbf4cbaf68ac0
Jody Clements <jody@neomorphic.co.uk>
2022-05-02 15:16:27 -0400

    Adds code to fetch and Store FlyEM token

--- src/actions/user.js
+++ src/actions/user.js
@@ -18,10 +18,25 @@ export default function setUserRoles(user) {
   };
 }
 
+function storeFlyEMToken(user, clioUrl) {
+  // TODO: get FlyEM token from clio store and save it to localStorage
+  const tokenUrl = `${clioUrl}/server/token`;
+  const options = {
+    method: 'post',
+    headers: {
+      Authorization: `Bearer ${user.token}`,
+    },
+  };
+  fetch(tokenUrl, options);
+  // now store it in localStorage or a cookie.
+}
+
 export function loginGoogleUser(user) {
-  return (dispatch) => {
+  return (dispatch, getState) => {
+    const clioUrl = getState().clio.get('projectUrl');
+    storeFlyEMToken(user, clioUrl);
+
     localStorage.setItem('user', JSON.stringify(user));
-    // TODO: get FlyEM token from clio store and save it to localStorage
     dispatch(setUserRoles(user));
     dispatch({
       type: 'LOGIN_GOOGLE_USER',
